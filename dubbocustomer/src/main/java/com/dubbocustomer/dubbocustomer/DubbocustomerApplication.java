d080a02a8abf4cf784a801b72cb6c95c59fe0da1
liuhongshan <liuhongshan@sinovatch.com>
2019-10-15 16:37:14 +0800

    dubbo demo 测试优化

--- dubbocustomer/src/main/java/com/dubbocustomer/dubbocustomer/DubbocustomerApplication.java
+++ dubbocustomer/src/main/java/com/dubbocustomer/dubbocustomer/DubbocustomerApplication.java
@@ -3,9 +3,14 @@ package com.dubbocustomer.dubbocustomer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 消费者
+ * @auther: liuhongshan
+ * @mail: liuhongshan@sinovatech.com
+ * @Date:  2019/10/15 11:12
+ */
 @SpringBootApplication
 public class DubbocustomerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DubbocustomerApplication.class, args);
     }
--- dubboserver/src/main/java/com/dobboserver/dubboserver/DubboserverApplication.java
+++ dubboserver/src/main/java/com/dobboserver/dubboserver/DubboserverApplication.java
@@ -3,6 +3,13 @@ package com.dobboserver.dubboserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * DubboserverApplication
+ * @auther: liuhongshan
+ * @mail: liuhongshan@sinovatech.com
+ * @Date:  2019/10/15 10:56
+ */
 @SpringBootApplication
 public class DubboserverApplication {
 
